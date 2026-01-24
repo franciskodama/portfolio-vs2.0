32da178624a08d2c2b0a0805417c8f1b9c3e72a2
Francis Kodama <fk@fkodama.com>
2026-01-24 09:30:41 -0500

    Navbar is tailwing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Link } from 'react-scroll';
 
-import './Navbar.css';
 import Logo from '../assets/images/fk-white-transparent-bg.png';
 import Menu from '../assets/images/menu-hamburguer.svg';
 import Close from '../assets/images/card-icon-close-white.svg';
@@ -30,19 +29,21 @@ const Navbar = () => {
   useEffect(() => {}, [isActive, isOpen]);
 
   return (
-    <header className='navbar'>
-      <div className='navbar-container'>
+    <header className='fixed w-full top-0 z-50 bg-dark overflow-visible'>
+      <div className='container mx-auto w-full lg-custom:w-[90%] flex justify-between items-center'>
         <Link to='hero' spy={true} smooth={true} offset={0} duration={500}>
-          <div className='py-2'>
-            <Image className='nav-logo' alt='logo fkodama' src={Logo} />
+          <div className='py-4 cursor-pointer'>
+            <Image
+              className='w-[100px] transform transition-transform duration-300 hover:scale-105'
+              alt='logo fkodama'
+              src={Logo}
+            />
           </div>
         </Link>
 
-        {/* -------- NAVBAR TOGGLE -------- */}
-
-        <div className='menu-toggle'>
+        <div className='relative flex flex-col justify-center z-99 cursor-pointer lg-custom:hidden'>
           <Image
-            className='menu-icon'
+            className='w-[30px] transform transition-transform duration-300 hover:scale-105'
             style={{
               display: isOpen ? 'none' : 'block',
             }}
@@ -51,7 +52,7 @@ const Navbar = () => {
             alt='hamburguer icon menu'
           />
           <Image
-            className='menu-icon'
+            className='w-[30px] transform transition-transform duration-300 hover:scale-105'
             style={{
               display: isOpen ? 'block' : 'none',
             }}
@@ -61,198 +62,61 @@ const Navbar = () => {
           />
         </div>
 
-        {/* -------- NAVBAR EXTENDED -------- */}
-
         <nav
-          className='nav-menu-mobile'
-          style={{ display: isActive ? 'block' : 'none' }}
+          className='absolute top-[4.2em] right-0 w-full bg-third flex flex-col items-end pr-8 pb-4 md-custom:w-[50%] md-custom:p-[5em_5em_3em_5em] lg-custom:hidden'
+          style={{ display: isActive ? 'flex' : 'none' }}
         >
-          <Link
-            className='nav-link-mobile'
-            to='reason'
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={2000}
-            onClick={handleClickOnLink}
-          >
-            this.portfolio
-          </Link>
-          <Link
-            className='nav-link-mobile'
-            to='about'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            about.me
-          </Link>
-          <Link
-            className='nav-link-mobile'
-            to='whyme'
-            spy={true}
-            smooth={true}
-            offset={100}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            why.me
-          </Link>
-          <Link
-            className='nav-link-mobile'
-            to='ai'
-            spy={true}
-            smooth={true}
-            offset={100}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            a.i.
-          </Link>
-          <Link
-            className='nav-link-mobile'
-            to='projects'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            projects
-          </Link>
-
-          <Link
-            className='nav-link-mobile'
-            to='api'
-            spy={true}
-            smooth={true}
-            offset={-150}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            api
-          </Link>
-
-          <Link
-            className='nav-link-mobile'
-            to='code'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            my.code
-          </Link>
-          <Link
-            className='nav-link-mobile'
-            to='contact'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            contact
-          </Link>
+          {[
+            { to: 'reason', label: 'this.portfolio', offset: -50 },
+            { to: 'about', label: 'about.me', offset: 0 },
+            { to: 'whyme', label: 'why.me', offset: 100 },
+            { to: 'ai', label: 'a.i.', offset: 100 },
+            { to: 'projects', label: 'projects', offset: 0 },
+            { to: 'api', label: 'api', offset: -150 },
+            { to: 'code', label: 'my.code', offset: 0 },
+            { to: 'contact', label: 'contact', offset: 0 },
+          ].map((link) => (
+            <Link
+              key={link.to}
+              className='relative font-main-regular text-bright text-[1.3rem] no-underline cursor-pointer block transform-gpu transition-all duration-300 mb-4 mr-4 hover:scale-105'
+              to={link.to}
+              spy={true}
+              smooth={true}
+              offset={link.offset}
+              duration={link.to === 'reason' ? 2000 : 500}
+              onClick={handleClickOnLink}
+            >
+              {link.label}
+            </Link>
+          ))}
         </nav>
 
         {/* -------- NAVBAR HORIZONTAL -------- */}
 
-        <nav className='nav-menu-desktop'>
-          <Link
-            className='nav-link-desktop'
-            to='reason'
-            spy={true}
-            smooth={true}
-            offset={-50}
-            duration={2000}
-            onClick={handleClickOnLink}
-          >
-            this.portfolio
-          </Link>
-          <Link
-            className='nav-link-desktop'
-            to='about'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            about.me
-          </Link>
-          <Link
-            className='nav-link-desktop'
-            to='whyme'
-            spy={true}
-            smooth={true}
-            offset={100}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            why.me
-          </Link>
-          <Link
-            className='nav-link-desktop'
-            to='ai'
-            spy={true}
-            smooth={true}
-            offset={100}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            {' '}
-            a.i.{' '}
-          </Link>
-          <Link
-            className='nav-link-desktop'
-            to='projects'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            {' '}
-            projects{' '}
-          </Link>
-
-          <Link
-            className='nav-link-desktop'
-            to='api'
-            spy={true}
-            smooth={true}
-            offset={-150}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            api
-          </Link>
-          <Link
-            className='nav-link-desktop'
-            to='code'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            my.code
-          </Link>
-          <Link
-            className='nav-link-desktop'
-            to='contact'
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={500}
-            onClick={handleClickOnLink}
-          >
-            contact
-          </Link>
+        <nav className='hidden lg-custom:grid grid-cols-[8rem_6.6rem_5.5rem_3.3rem_5.8rem_3.6rem_6rem_4.3rem] items-center justify-end w-[40em] mr-[5em]'>
+          {[
+            { to: 'reason', label: 'this.portfolio', offset: -50 },
+            { to: 'about', label: 'about.me', offset: 0 },
+            { to: 'whyme', label: 'why.me', offset: 100 },
+            { to: 'ai', label: 'a.i.', offset: 100 },
+            { to: 'projects', label: 'projects', offset: 0 },
+            { to: 'api', label: 'api', offset: -150 },
+            { to: 'code', label: 'my.code', offset: 0 },
+            { to: 'contact', label: 'contact', offset: 0 },
+          ].map((link) => (
+            <Link
+              key={link.to}
+              className='block relative text-bright no-underline cursor-pointer transition-all duration-300 font-main-regular text-[1rem] hover:scale-105 hover:first-letter:text-third'
+              to={link.to}
+              spy={true}
+              smooth={true}
+              offset={link.offset}
+              duration={link.to === 'reason' ? 2000 : 500}
+              onClick={handleClickOnLink}
+            >
+              {link.label}
+            </Link>
+          ))}
         </nav>
       </div>
     </header>
