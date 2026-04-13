ddeea28df433228df09135fdbb3fd19077049c04
Francis Kodama <fk@fkodama.com>
2026-04-13 14:42:18 -0400

    Limited version

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -106,9 +106,9 @@ const About = () => {
         </div>
 
         <div className='relative flex flex-col w-full justify-center max-w-[36em] px-4 overflow-hidden mb-12 md-custom:pl-[10em] md-custom:max-w-[41em] md-custom:items-start lg-custom:pl-16 lg-custom:mb-0 lg-custom:w-1/2 lg-custom:max-w-[36em]'>
-          <div className='absolute top-12 left-[-5.2em] font-main-light text-[0.8rem] w-[20em] leading-[1.2em] p-2 text-bright bg-third -rotate-45 text-center'>
-            <p className='font-main-semibold'>NOW WITH </p>
-            <p className='font-main-semibold'>BUILT-IN ARCHITECTURE</p>
+          <div className='absolute top-12 left-[-5.2em] font-main-light text-[0.9rem] w-[20em] leading-[1.2em] p-2 text-bright bg-third -rotate-45 text-center'>
+            {/* <p className='font-main-semibold'>UPGRADED VERSION</p> */}
+            <p className='font-main-semibold'>LIMITED EDITION</p>
           </div>
 
           <div
