cb6763df6ffd8c328199b617ab3cc08a041fec56
Francis Kodama <fk@fkodama.com>
2026-05-05 15:55:31 -0400

    This is the end

--- src/components/Philosophy.tsx
+++ src/components/Philosophy.tsx
@@ -47,49 +47,49 @@ const Philosophy = () => {
 
         <Parallax opacity={[0, 2]}>
           <ul className='lg-custom:scale-65 -mb-20 mt-24 md-custom:-mt-28 font-main-heavy uppercase text-center animate-words-appearance md-custom:w-[28em] md-custom:text-[2em] md-custom:leading-10 mx-auto'>
-            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] pr-60 text-third text-[2rem] leading-10 md-custom:text-[6.4rem] md-custom:leading-34'>
+            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] md-custom:pr-60 text-third text-[2rem] leading-10 md-custom:text-[6.4rem] md-custom:leading-34'>
               Authentic
             </li>
-            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] pr-32 text-third text-[2rem] leading-10 md-custom:text-[6rem] md-custom:leading-21'>
+            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] md-custom:pr-32 text-third text-[2rem] leading-10 md-custom:text-[6rem] md-custom:leading-21'>
               Leadership
             </li>
-            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] pr-66 text-bright text-[2.5rem] leading-10 md-custom:text-[7.5rem] md-custom:leading-42'>
+            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] md-custom:pr-66 text-bright text-[2.5rem] leading-10 md-custom:text-[7.5rem] md-custom:leading-42'>
               PRODUCT
             </li>
-            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] pr-16 text-bright text-[2.2rem] leading-10 md-custom:text-[7rem] md-custom:leading-24'>
+            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] md-custom:pr-16 text-bright text-[2.2rem] leading-10 md-custom:text-[7rem] md-custom:leading-24'>
               DISCOVERY
             </li>
-            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1.6rem] leading-[1.8rem] text-left pl-6 md-custom:text-[4.5rem] md-custom:leading-28 md-custom:pl-32'>
+            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1.6rem] leading-[1.8rem] text-center md-custom:text-left md-custom:pl-32 md-custom:text-[4.5rem] md-custom:leading-28'>
               OUTCOMES
             </li>
-            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1.6rem] leading-[1.45rem] text-left pl-[2em] md-custom:text-[4.5rem] md-custom:leading-14 md-custom:pl-32'>
+            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1.6rem] leading-[1.45rem] text-center md-custom:text-left md-custom:pl-32 md-custom:text-[4.5rem] md-custom:leading-14'>
               OVER OUTPUTS
             </li>
-            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-bright text-[1.1rem] leading-[1.3rem] text-left pl-[2em] md-custom:text-[3.4rem] md-custom:leading-24 md-custom:pl-[2em]'>
+            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-bright text-[1.1rem] leading-[1.3rem] text-center md-custom:text-left md-custom:pl-[2em] md-custom:text-[3.4rem] md-custom:leading-24'>
               TECHNICAL FEASIBILITY
             </li>
-            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1rem] leading-[1.2rem] text-left pl-[5em] md-custom:text-[3.5rem] md-custom:leading-11 md-custom:pl-[4em]'>
+            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1rem] leading-[1.2rem] text-center md-custom:text-left md-custom:pl-[4em] md-custom:text-[3.5rem] md-custom:leading-11'>
               MINIMAL VIABLE
             </li>
-            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1.3rem] leading-6 text-left pl-[4em] md-custom:text-[3.9rem] md-custom:leading-22 md-custom:pl-[3.6em]'>
+            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1.3rem] leading-6 text-center md-custom:text-left md-custom:pl-[3.6em] md-custom:text-[3.9rem] md-custom:leading-22'>
               ARCHITECTURE
             </li>
-            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-bright text-[1.35rem] leading-[1.6rem] md-custom:text-[6rem] md-custom:leading-28 md-custom:pr-[4em]'>
+            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-bright text-[1.35rem] leading-[1.6rem] text-center md-custom:text-right md-custom:pr-[4em] md-custom:text-[6rem] md-custom:leading-28'>
               USER
             </li>
-            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-bright text-[1.35rem] leading-[1.6rem] pl-[1em] md-custom:text-[6rem] md-custom:leading-28 md-custom:pl-[2em]'>
+            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-bright text-[1.35rem] leading-[1.6rem] text-center md-custom:text-left md-custom:pl-[2em] md-custom:text-[6rem] md-custom:leading-28'>
               EXPERIENCE
             </li>
-            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1.1rem] leading-6 pl-4 md-custom:text-[4rem] md-custom:leading-19 md-custom:pr-[1.5em]'>
+            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-third text-[1.1rem] leading-6 text-center md-custom:pr-[1.5em] md-custom:text-[4rem] md-custom:leading-19'>
               SCALABLE
             </li>
-            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-third text-[1.4rem] leading-[1.8rem] text-left pl-10 md-custom:text-[4rem] md-custom:leading-20 md-custom:pl-[4.2em]'>
+            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-third text-[1.4rem] leading-[1.8rem] text-center md-custom:text-left md-custom:pl-[4.2em] md-custom:text-[4rem] md-custom:leading-20'>
               INTEGRITY
             </li>
-            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-bright text-[1.6rem] leading-[1.8rem] text-right pr-6 md-custom:text-[6.5rem] md-custom:leading-30 md-custom:pl-[2.4em]'>
+            <li className='transform-[skew(0deg,-30deg)_scaleY(1.33333)] text-bright text-[1.6rem] leading-[1.8rem] text-center md-custom:text-right md-custom:pl-[2.4em] md-custom:text-[6.5rem] md-custom:leading-30'>
               EMPOWERED
             </li>
-            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-bright text-[2rem] leading-10 text-left pl-[1.5em] md-custom:text-[6.5rem] md-custom:leading-30 md-custom:pl-[2.9em]'>
+            <li className='transform-[skew(60deg,-30deg)_scaleY(0.66667)] text-bright text-[2rem] leading-10 text-center md-custom:text-left md-custom:pl-[2.9em] md-custom:text-[6.5rem] md-custom:leading-30'>
               EXECUTION
             </li>
           </ul>
