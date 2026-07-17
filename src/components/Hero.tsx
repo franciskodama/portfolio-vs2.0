6958ce77786328872c61eedc9509a7644b890135
Francis Kodama <fk@fkodama.com>
2026-07-16 20:26:26 -0400

    Curlybraces

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ gsap.registerPlugin(ScrollTrigger);
 import SquareBracketsImg from '../assets/images/hero/hero_3d_square_brackets.png';
 import CodeBracketsImg from '../assets/images/hero/hero_3d_code_brackets.png';
 // import CurlyBracesBiggerImg from '../assets/images/hero/hero_3d_curly_braces2.webp';
-import CurlyBracesBiggerImg from '../assets/images/hero/hero_3d_curly_braces2.png';
+import CurlyBracesBiggerImg from '../assets/images/hero/hero_3d_curly_braces_top_right.png';
 import CurlyBracesImg from '../assets/images/hero/hero_3d_curly_braces.png';
 import Scroll from '../assets/images/ico-scroll.svg';
 
